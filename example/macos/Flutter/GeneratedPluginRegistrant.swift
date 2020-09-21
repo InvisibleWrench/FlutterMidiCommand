7db70ded4ad6d2ca903c5f815b52f04f246b5d9b
Morten Mortensen <morten@invisiblewrench.com>
2020-09-21 12:15:57 +0200

    added links to macos and linux packages

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_midi_command_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterMidiCommandMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterMidiCommandMacosPlugin"))
 }
