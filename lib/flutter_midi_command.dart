aae36d55dfe5ad882f9dcf041bb872a0b598dd3e
morten <morten@invisiblewrench.com>
2020-11-13 13:59:01 +0100

    linux

--- lib/flutter_midi_command.dart
+++ lib/flutter_midi_command.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'dart:typed_data';
+import 'package:flutter_midi_command_linux/flutter_midi_command_linux.dart';
 import 'package:flutter_midi_command_platform_interface/flutter_midi_command_platform_interface.dart';
 export 'package:flutter_midi_command_platform_interface/flutter_midi_command_platform_interface.dart'
-    show MidiDevice, MidiPacket;
+    show MidiDevice, MidiPacket, MidiPort;
 
 class MidiCommand {
   factory MidiCommand() {
@@ -17,62 +19,75 @@ class MidiCommand {
 
   static MidiCommand _instance;
 
-  static MidiCommandPlatform get _platform => MidiCommandPlatform.instance;
+  static MidiCommandPlatform  __platform;
 
-  /// Gets a list of available MIDI devices and returns it.
-  Future<List<MidiDevice>> get devices async {
-    return _platform.devices;
+  static MidiCommandPlatform get _platform {
+    if (__platform != null)
+      return __platform;
+
+    if (Platform.isLinux) {
+      __platform = FlutterMidiCommandLinux();
+    } else {
+      __platform = MidiCommandPlatform.instance;
+    }
+    return __platform;
   }
 
-  /// Starts scanning for BLE MIDI devices.
-  ///
-  /// Found devices will be included in the list returned by [devices].
-  Future<void> startScanningForBluetoothDevices() async {
-    return _platform.startScanningForBluetoothDevices();
+    /// Gets a list of available MIDI devices and returns it.
+    Future<List<MidiDevice>> get devices async {
+      return _platform.devices;
+    }
+
+    /// Starts scanning for BLE MIDI devices.
+    ///
+    /// Found devices will be included in the list returned by [devices].
+    Future<void> startScanningForBluetoothDevices() async {
+      return _platform.startScanningForBluetoothDevices();
+    }
+
+    /// Stop scanning for BLE MIDI devices.
+    void stopScanningForBluetoothDevices() {
+      _platform.stopScanningForBluetoothDevices();
+    }
+
+    /// Connects to the device.
+    void connectToDevice(MidiDevice device) {
+      _platform.connectToDevice(device);
+    }
+
+    /// Opens a port on a connected device.
+    void openPortsOnDevice(MidiDevice device, List<MidiPort> ports) {
+      _platform.openPortsOnDevice(device, ports);
+    }
+
+    /// Disconnects from the device.
+    void disconnectDevice(MidiDevice device) {
+      _platform.disconnectDevice(device);
+    }
+
+    void teardown() {
+      _platform.teardown();
+    }
+
+    /// Sends data to the currently connected device.
+    ///
+    /// Data is an UInt8List of individual MIDI command bytes.
+    void sendData(Uint8List data, {String deviceId, int timestamp}) {
+      _platform.sendData(data, deviceId: deviceId, timestamp: timestamp);
+    }
+
+    /// Stream firing events whenever a midi package is received.
+    ///
+    /// The event contains the raw bytes contained in the MIDI package.
+    Stream<MidiPacket> get onMidiDataReceived {
+      return _platform.onMidiDataReceived;
+    }
+
+    /// Stream firing events whenever a change in the MIDI setup occurs.
+    ///
+    /// For example, when a new BLE devices is discovered.
+    Stream<String> get onMidiSetupChanged {
+      return _platform.onMidiSetupChanged;
+    }
   }
 
-  /// Stop scanning for BLE MIDI devices.
-  void stopScanningForBluetoothDevices() {
-    _platform.stopScanningForBluetoothDevices();
-  }
-
-  /// Connects to the device.
-  void connectToDevice(MidiDevice device) {
-    _platform.connectToDevice(device);
-  }
-
-  /// Opens a port on a connected device.
-  void openPortsOnDevice(MidiDevice device, List<MidiPort> ports) {
-    _platform.openPortsOnDevice(device, ports);
-  }
-
-  /// Disconnects from the device.
-  void disconnectDevice(MidiDevice device) {
-    _platform.disconnectDevice(device);
-  }
-
-  void teardown() {
-    _platform.teardown();
-  }
-
-  /// Sends data to the currently connected device.
-  ///
-  /// Data is an UInt8List of individual MIDI command bytes.
-  void sendData(Uint8List data, {String deviceId, int timestamp}) {
-    _platform.sendData(data, deviceId: deviceId, timestamp: timestamp);
-  }
-
-  /// Stream firing events whenever a midi package is received.
-  ///
-  /// The event contains the raw bytes contained in the MIDI package.
-  Stream<MidiPacket> get onMidiDataReceived {
-    return _platform.onMidiDataReceived;
-  }
-
-  /// Stream firing events whenever a change in the MIDI setup occurs.
-  ///
-  /// For example, when a new BLE devices is discovered.
-  Stream<String> get onMidiSetupChanged {
-    return _platform.onMidiSetupChanged;
-  }
-}
