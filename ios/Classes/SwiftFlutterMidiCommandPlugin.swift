199fd3eaa59ff21bd9ef5eae586f4390d6823646
Morten Boye Mortensen <morten@invisiblewrench.com>
2024-04-10 11:04:25 +0200

    fix

--- ios/Classes/SwiftFlutterMidiCommandPlugin.swift
+++ ios/Classes/SwiftFlutterMidiCommandPlugin.swift
@@ -1341,7 +1341,7 @@ class ConnectedOwnVirtualDevice : ConnectedVirtualOrNativeDevice {
         let packetList = UnsafeMutablePointer<MIDIPacketList>.allocate(capacity: 1)
         var packet = MIDIPacketListInit(packetList)
         let time = timestamp ?? mach_absolute_time()
-        packet = MIDIPacketListAdd(packetList, 1024, packet, time, bytes.count, bytes)
+        packet = MIDIPacketListAdd(packetList, MemoryLayout.size(ofValue: packetList), packet, time, bytes.count, bytes)
 
         let status = MIDIReceived(virtualSourceEndpoint, packetList)
         if(status != noErr){
